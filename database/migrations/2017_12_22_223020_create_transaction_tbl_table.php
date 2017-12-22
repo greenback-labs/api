209ebf39f1582ff4027157dc857304faac84f7a0
napoleaofw <napoleao.wingert@gmail.com>
2017-12-22 21:15:35 -0200

    add migration

---
+++ database/migrations/2017_12_22_223020_create_transaction_tbl_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionTblTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_tbl', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('account_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->integer('person_id')->unsigned()->nullable();
+            $table->string('type');
+            $table->decimal('value');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->timestamp('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_tbl');
+    }
+}
