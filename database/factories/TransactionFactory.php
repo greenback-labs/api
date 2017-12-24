63f23c7e401a46c8724e94f83a0adedc33ec8bfb
napoleaofw <napoleao.wingert@gmail.com>
2017-12-23 22:37:51 -0200

    change factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        'type' =>  $faker->randomElement(['entry', 'exit']),
+        'type' =>  $faker->randomElement(['in', 'out']),
         'value' => $faker->optional($weight = 0.95, $default = $faker->randomFloat(2, 500, 10000))->randomFloat(2, 0.01, 500),
         'title' => $faker->sentence(rand(2, 6)),
         'description' => $faker->optional($weight = 0.5)->paragraph(rand(1, 6)),
