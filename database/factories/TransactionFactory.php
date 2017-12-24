f265d32ece861d9edebe54d3d367204ea663cd1b
napoleaofw <napoleao.wingert@gmail.com>
2017-12-24 15:35:50 -0200

    change factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -9,6 +9,6 @@ $factory->define(Transaction::class, function (Faker $faker) {
         'value' => $faker->optional($weight = 0.95, $default = $faker->randomFloat(2, 500, 10000))->randomFloat(2, 0.01, 500),
         'title' => $faker->sentence(rand(2, 6)),
         'description' => $faker->optional($weight = 0.5)->paragraph(rand(1, 6)),
-        'date' => ? $faker->date()
+        'date' => $faker->date()
     ];
 });
