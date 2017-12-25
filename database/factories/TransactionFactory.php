c2f4d1215aa30cfe983f257f82fa131f275fe7d1
napoleaofw <napoleao.wingert@gmail.com>
2017-12-25 20:57:59 -0200

    change factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -5,7 +5,7 @@ use App\Transaction;
 
 $factory->define(Transaction::class, function (Faker $faker) {
     return [
-        'type' =>  $faker->randomElement(['in', 'out']),
+        'type' => substr($faker->randomElement(['in', 'out']), 1, 10),
         'value' => round($faker->optional($weight = 0.95, $default = $faker->randomFloat(2, 500, 10000))->randomFloat(2, 0.01, 500), 2),
         'title' => substr($faker->sentence(rand(2, 6)), 1, 250),
         'description' => substr($faker->optional($weight = 0.5)->paragraph(rand(1, 6)), 1, 1000),
