1dc0084e4a7e8c1b67d586ba88e910704418f484
napoleaofw <napoleao.wingert@gmail.com>
2017-12-25 19:38:43 -0200

    change seeder

--- database/seeds/InstallmentTableSeeder.php
+++ database/seeds/InstallmentTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Installment;
+use App\Transaction;
 
 class InstallmentTableSeeder extends Seeder
 {
@@ -11,6 +13,28 @@ class InstallmentTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $minPerTransaction = 2;
+        $maxPerTransaction = 12;
+
+        foreach(Transaction::whereNull('transfer_code')->get() as $recordTransaction) {
+            if(rand(1, 3) % 2 === 0 && $recordTransaction->value > 1) {
+                $installmentQuantity = rand($minPerTransaction, $maxPerTransaction);
+                $period = array_rand(['day', 'month', 'year']);
+                $currentDate = $recordTransaction->date;
+                $installmentValue = round($recordTransaction->value / $installmentQuantity, 2);
+                $lastInstallmentValue = $installmentValue + ($recordTransaction->value - ($installmentValue * $installmentQuantity));
+                $currentInstallment = 1;
+
+                factory(Installment::class, $installmentQuantity)->create()->each(function($recordInstallment) {
+                    $recordInstallment->transaction_id = $recordTransaction->id;
+                    $recordInstallment->deadline_date = $currentDate;
+                    $recordInstallment->effective_date = $currentDate;
+                    $recordInstallment->value = $currentInstallment === $installmentQuantity ? $lastInstallmentValue : $installmentValue;
+
+                    $currentDate = strtotime('+1 ' . $period, $currentDate);
+                    $currentInstallment += 1;
+                });
+            }
+        }
     }
 }
