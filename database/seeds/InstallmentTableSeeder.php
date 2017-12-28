736b00260ee08e7fbba66bd0c8704005374eb1f6
napoleaofw <napoleao.wingert@gmail.com>
2017-12-28 20:31:29 -0200

    change seeder

--- database/seeds/InstallmentTableSeeder.php
+++ database/seeds/InstallmentTableSeeder.php
@@ -19,27 +19,30 @@ class InstallmentTableSeeder extends Seeder
         $periodList = ['day', 'month', 'year'];
 
         foreach(Transaction::whereNull('transfer_code')->get() as $recordTransaction) {
+            $installmentQuantity = 1;
+
             if(rand(1, 3) % 2 === 0 && $recordTransaction->value > 1) {
                 $installmentQuantity = rand($minPerTransaction, $maxPerTransaction);
-                $period = $periodList[array_rand($periodList)];
-                $currentDate = strtotime($recordTransaction->date);
-                $installmentValue = round($recordTransaction->value / $installmentQuantity, 2);
-                $lastInstallmentValue = $installmentValue + ($recordTransaction->value - ($installmentValue * $installmentQuantity));
-                $currentInstallment = 1;
+            }
+            
+            $period = $periodList[array_rand($periodList)];
+            $currentDate = strtotime($recordTransaction->date);
+            $installmentValue = round($recordTransaction->value / $installmentQuantity, 2);
+            $lastInstallmentValue = $installmentValue + ($recordTransaction->value - ($installmentValue * $installmentQuantity));
+            $currentInstallment = 1;
 
-                $recordsInstallment = factory(Installment::class, $installmentQuantity)->make();
+            $recordsInstallment = factory(Installment::class, $installmentQuantity)->make();
 
-                foreach($recordsInstallment as $recordInstallment) {
-                    $recordInstallment->transaction_id = $recordTransaction->id;
-                    $recordInstallment->deadline_date = date('Y-m-d H:i:s', $currentDate);
-                    $recordInstallment->effective_date = date('Y-m-d H:i:s', $currentDate);
-                    $recordInstallment->value = round($currentInstallment === $installmentQuantity ? $lastInstallmentValue : $installmentValue, 2);
-                    
-                    $recordInstallment->save();
+            foreach($recordsInstallment as $recordInstallment) {
+                $recordInstallment->transaction_id = $recordTransaction->id;
+                $recordInstallment->deadline_date = date('Y-m-d H:i:s', $currentDate);
+                $recordInstallment->effective_date = date('Y-m-d H:i:s', $currentDate);
+                $recordInstallment->value = round($currentInstallment === $installmentQuantity ? $lastInstallmentValue : $installmentValue, 2);
+                
+                $recordInstallment->save();
 
-                    $currentDate = strtotime('+1 ' . $period, $currentDate);
-                    $currentInstallment += 1;
-                }
+                $currentDate = strtotime('+1 ' . $period, $currentDate);
+                $currentInstallment += 1;
             }
         }
     }
