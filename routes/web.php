a86924415ccc4d6d444ada78e16d453c3766965d
napoleaofw <napoleao.wingert@gmail.com>
2017-12-30 22:32:31 -0200

    change route

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('accounts', 'AccountController');
-Route::resource('categories', 'CategoryController');
-Route::resource('installments', 'InstallmentController');
-Route::resource('persons', 'PersonController');
-Route::resource('transactions', 'TransactionController');
+Route::middleware(['cors'])->group(function () {
+    Route::resource('accounts', 'AccountController');
+    Route::resource('categories', 'CategoryController');
+    Route::resource('installments', 'InstallmentController');
+    Route::resource('persons', 'PersonController');
+    Route::resource('transactions', 'TransactionController');
+});
